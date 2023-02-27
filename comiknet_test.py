3e7970273a217fb3f2ccfd290bd5e0ee5efe7927
坤 徐 <me@biduang.cn>
2023-02-27 14:10:58 +0800

    PreRev 4.3.9 - 0227 测试组件更新

--- comiknet_test.py
+++ comiknet_test.py
@@ -1,19 +1,18 @@
-from time import sleep
 import pytest
 import requests
+import json
 import os
 
 def test_login():
     username = os.getenv("username")
     password = os.getenv("password")
 
-    res = requests.get("http://localhost:8000/login", data={
-        "username": username,
-        "password": password
-    })
-
+    res = requests.post("http://localhost:8000/login", data=json.dumps({
+        "username": username if username else "admin",
+        "password": password if password else "admin"
+    }))
+    
     assert res.status_code == 200
-    print(res.text)
 
 if __name__ == "__main__":
     test_login()
